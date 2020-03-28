4c166cc153cbc02050b63de6f6e13aab4611198f
Александр <alexandr.tvelenev@mail.ru>
2020-03-28 13:36:58 +0500

    firstCommit

--- auth-lib.js
+++ auth-lib.js
@@ -1,11 +1,37 @@
+'use strict';
+
+let allUsers = [];
+let allGroups = [];
+let allRights = [];
+
 // Возвращает массив всех пользователей.
-function users() {}
+function users() {
+    return allUsers;
+}
 
 //Создает нового пользователя с указанным логином username и паролем password, возвращает созданного пользователя.
-function createUser(name, password) {}
+function createUser(name, password) {
+    let user = {
+        name: name,
+        password: password
+    }
+
+    allUsers.push(user)
+    return user;
+}
 
 // Удаляет пользователя user
-function deleteUser(user) {}
+function deleteUser(user) {
+    if (!user) {
+        throw Error('hello');
+    }
+
+    if (allUsers.indexOf(user) === -1) {
+        throw Error('111111');
+    }
+
+    allUsers.splice(allUsers.indexOf(user), 1);
+}
 
 // Возвращает массив групп, к которым принадлежит пользователь user
 function userGroups(user) {}
@@ -17,22 +43,92 @@ function addUserToGroup(user, group) {}
 function removeUserFromGroup(user, group) {}
 
 // Возвращает массив прав
-function rights() {}
+function rights() {
+    // let rights = ['view content', 'edit content', 'delete content', 'add content'];
+    // rights.push(allRights);
+    return allRights;
+}
 
 // Создает новое право с именем name и возвращает его
-function createRight(name) {}
+function createRight(name) {
+    let right = {
+        name: name
+    }
+
+    allRights.push(right);
+    return right;
+}
 
 // Удаляет право right
-function deleteRight(right) {}
+function deleteRight(right) {
+    if (!right) {
+        throw Error('It is not a group');
+    }
+
+    if (allRights.indexOf(right) === -1) {
+        throw Error('the operation was performed earlier');
+    }
+
+    allRights.splice(allRights.indexOf(right), 1);
+}
 
 // Возвращает массив групп
-function groups() {}
+function groups() {
+    // let groups = [
+    //     {
+    //         "admin": [allRights[0], allRights[1], allRights[2], allRights[3]]
+    //     },
+    //     {
+    //         "developer": [allRights[1], allRights[3], allRights[2]],
+    //     },
+    //     {
+    //         "user": [allRights[0]]
+    //     }
+    //     // "admin": [allRights[0], allRights[1], allRights[2], allRights[3]],
+    //     // "developer": [allRights[1], allRights[3], allRights[2]],
+    //     // "user": [allRights[0]]
+    // ]
+
+
+    // groups.push(allGroups);
+    return allGroups;
+}
 
 // Создает новую группу и возвращает её.
-function createGroup(name) {}
+function createGroup(name) {
+    // let groups = [
+    //     {
+    //         "admin": [allRights[0], allRights[1], allRights[2], allRights[3]]
+    //     },
+    //     {
+    //         "developer": [allRights[1], allRights[3], allRights[2]],
+    //     },
+    //     {
+    //         "user": [allRights[0]]
+    //     }
+    // ];
+    // groups.push(allGroups);
+    // return groups;
+
+    let group = {
+        name: name
+    }
+    allGroups.push(group);
+    return group;
+}
 
 // Удаляет группу group
-function deleteGroup(group) {}
+function deleteGroup(group) {
+    if (!group) {
+        throw Error('It is not a group');
+    }
+
+    if (allGroups.indexOf(group) === -1) {
+        throw Error('the operation was performed earlier');
+    }
+
+    allGroups.splice(allGroups.indexOf(group), 1);
+}
 
 // Возвращает массив прав, которые принадлежат группе group
 function groupRights() {}
